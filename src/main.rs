fdce5f46b365a41429172f3c24fc55d18d04795a
DasLixou <einlixou@gmail.com>
2024-06-25 17:02:10 +0200

    Add dummy plot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use eframe::Frame;
-use egui::Context;
+use egui::{Context, RichText};
+use egui_plot::{Line, PlotPoint, Text};
 use gate::{Gate, GateType};
 
 mod gate;
@@ -23,8 +24,16 @@ impl Yoyo {
 impl eframe::App for Yoyo {
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("yoyo");
-            ui.add(Gate { ty: GateType::And });
+            // ui.add(Gate { ty: GateType::And });
+            egui_plot::Plot::new("yoyo_visualizer")
+                .show_axes(false)
+                .data_aspect(1.)
+                .allow_scroll(false)
+                .show_x(false)
+                .show_y(false)
+                .show(ui, |pui| {
+                    pui.text(Text::new(PlotPoint::new(15, 13), RichText::new("clk")));
+                })
         });
     }
 }
