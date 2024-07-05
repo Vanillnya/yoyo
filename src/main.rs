f2ed722e2297518361ff25b8a357e3697de03ad2
DasLixou <einlixou@gmail.com>
2024-07-05 22:05:07 +0200

    remove border

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use eframe::Frame;
-use egui::{Context, RichText};
-use egui_plot::{Line, PlotPoint, Text};
-use gate::{Gate, GateType};
+use egui::{Context, Frame, RichText};
+use egui_plot::{PlotPoint, Text};
 
 mod gate;
 
@@ -22,18 +20,20 @@ impl Yoyo {
 }
 
 impl eframe::App for Yoyo {
-    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // ui.add(Gate { ty: GateType::And });
-            egui_plot::Plot::new("yoyo_visualizer")
-                .show_axes(false)
-                .data_aspect(1.)
-                .allow_scroll(false)
-                .show_x(false)
-                .show_y(false)
-                .show(ui, |pui| {
-                    pui.text(Text::new(PlotPoint::new(15, 13), RichText::new("clk")));
-                })
-        });
+    fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default()
+            .frame(Frame::none())
+            .show(ctx, |ui| {
+                // ui.add(Gate { ty: GateType::And });
+                egui_plot::Plot::new("yoyo_visualizer")
+                    .show_axes(false)
+                    .data_aspect(1.)
+                    .allow_scroll(false)
+                    .show_x(false)
+                    .show_y(false)
+                    .show(ui, |pui| {
+                        pui.text(Text::new(PlotPoint::new(15, 13), RichText::new("clk")));
+                    })
+            });
     }
 }
